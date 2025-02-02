2762d07d8d985a9a42ef9ae215d9f08fdbab2af2
j0j1j2 <jaeyoung.choi@docenty.ai>
2025-02-02 17:03:10 +0900

    feat: stop windows supports

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const createBeforeQuitHandler = app => {
         title: 'Quit Hyper?',
         message: 'Quit Hyper?',
         detail: 'All sessions will be closed.'
-      }, index => {
-        if (index === 0) {
+      }).then(({response}) => {
+        if (response === 0) {
           quitConfirmed = true;
           app.quit();
         }
@@ -25,64 +25,21 @@ const createBeforeQuitHandler = app => {
   };
 };
 
-exports.decorateConfig = config => {
-  if (typeof config.confirmQuit !== undefined) {
-    confirmQuit = Boolean(config.confirmQuit);
-  }
-
-  return config;
-};
 
 exports.onApp = app => {
   switch (process.platform) {
     case 'win32':
-      // Windows confirmations happen in the middleware
+      // Windows not supported
       break;
-    default:
-      if (beforeQuitHandler) {
-        app.off('before-quit', beforeQuitHandler);
+      default:
+        if (beforeQuitHandler) {
+          app.off('before-quit', beforeQuitHandler);
+        }
+        
+        beforeQuitHandler = createBeforeQuitHandler(app);
+        app.on('before-quit', beforeQuitHandler);
+        
+        break;
       }
-
-      beforeQuitHandler = createBeforeQuitHandler(app);
-      app.on('before-quit', beforeQuitHandler);
-
-      break;
-  }
-};
-
-exports.middleware = _ => next => action => {
-  const {dialog, app} = require('electron').remote;
-
-  let confirmQuit = true;
-
-  if (typeof app.config.getConfig().confirmQuit !== undefined) {
-    confirmQuit = Boolean(app.config.getConfig().confirmQuit);
-  }
-
-  switch (process.platform) {
-    case 'win32':
-      if (confirmQuit && action.type === 'UI_WINDOW_CLOSE') {
-        dialog.showMessageBox({
-          type: 'question',
-          buttons: ['OK', 'Cancel'],
-          defaultId: 0,
-          title: 'Quit Hyper?',
-          message: 'Quit Hyper?',
-          detail: 'All sessions will be closed.'
-        }, index => {
-          if (index === 0) {
-            next(action);
-          }
-        });
-
-        return;
-      }
-
-      break;
-    default:
-      // No special middleware for non-windows operating systems
-      break;
-  }
-
-  next(action);
-};
+    };
+  
